e4272883dbe410a833c32ed2cee74f085330603a
jordanfurr <ejfurr@umich.edu>
2024-06-28 18:36:08 -0400

    champions

--- pages/champions.vue
+++ pages/champions.vue
@@ -1,34 +1,51 @@
 <template>
-    <div class="ph5 pv6 h-90vh">
+    <div class="ph5 pv6">
         <div class="flex flex-row space-between">
-            <div class="mb7 w-25">
-                <p class="boldspace">EARNEST CHAMPIONS</p>
-                <br>
-                <p class="boldspace mb2 yellow">ARTISTS</p>
-                <p></p>
-                <p>...</p>
-                <br>
-                <p class="boldspace mb2 yellow">FOOD</p>
-                <p>Holybelly Paris</p>
-                <p>...</p>
-                <br>
-                <p class="boldspace mb2">MUSICIANS</p>
-                <p>...</p>
-                <p>...</p>
-                <br>
-                <p class="boldspace mb2">MAKERS</p>
-                <p>...</p>
-                <p>...</p>
-                <br>
-                <p class="boldspace mb2">PROJECTS</p>
-                <p>...</p>
-                <p>Center For Expanding Compassion</p>
-                <br>
+            <div class="mb7 w-30">
+                <p class="boldspace mb3">EARNEST CHAMPIONS</p>
+                <div class="overflow-y champions">
+                    <br>
+                    <p class="boldspace mb2 yellow">WRITERS & POETS</p>
+                    <p>Naomi Shihab Nye</p>
+                    <p>...</p>
+                    <br>
+                    <p class="boldspace mb2 yellow">ARTISTS</p>
+                    <p>Lynne Curran & David Swift</p>
+                    <p>...</p>
+                    <br>
+                    <p class="boldspace mb2 yellow">MUSICIANS</p>
+                    <p>Eric Tinkerhess</p>
+                    <p>Ezra Geo</p>
+                    <br>
+                    <p class="boldspace mb2 yellow">MAKERS</p>
+                    <p>Annemarie O'Sullivan & Tom McWalter</p>
+                    <p>E.C.one</p>
+                    <br>
+                    <p class="boldspace mb2 yellow">PROJECTS</p>
+                    <p>La Tuile à Loup</p>
+                    <p>Center For Expanding Compassion</p>
+                    <br>
+                    <p class="boldspace mb2 yellow">BUSINESSES</p>
+                    <p>Hiut Denim</p>
+                    <p>Made With Harmony</p>
+                    <br>
+                    <p class="boldspace mb2 yellow">FOOD</p>
+                    <p>Holybelly Cafe</p>
+                    <p>...</p>
+                    <br>
+                    <br>
+                    <p>*There is a lot of overlap in these categories</p>
+                    <br>
+                </div>
+            </div>
+            <div class="w-45 text-center pa5">
+                <img class="w-100" src="/img/earnest.jpg" alt="">
             </div>
             <div class="w-25">
-                <p class="boldspace">WHAT?</p>
+                <p class="boldspace mb3">WHAT?</p>
                 <br>
-                <p>A key component of The Earnest Project is highlighting people and projects that inspire us to stay earnest.</p>
+                <p>A key component of The Earnest Project is highlighting people and projects that inspire us to stay
+                    earnest.</p>
                 <br>
                 <p>This is a growing list. Please submit more earnestness.</p>
             </div>
