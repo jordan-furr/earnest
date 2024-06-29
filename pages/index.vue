83c553cc85d164c9c752f2ed4e62ee2fab3b3c36
jordanfurr <ejfurr@umich.edu>
2024-06-29 10:00:03 -0400

    authentic hover

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,44 @@
 <template>
     <div class="ph5 pv6 h-90vh">
         <div class="w-100 flex flex-row">
-            <div class="w-25">
+            <div class="w-20">
                 <div class="w-90">
                     <values-menu />
                 </div>
             </div>
-            <div class="text-center w-50 mt6">
-                
-                <img class="w-100" src="/img/Earnest Venn Diagram.png" alt="">
+            <div class="text-center w-60 mt6 ph3">
+                <div class="flex flex-col">
+                    <div>
+                        <p class="home-word">wholehearted</p>
+                    </div>
+                    <div class="flex flex-row">
+                        <div class="w-30 flex flex-col space-around ">
+                            <div class="text-right hover-auth">
+                                <p class="home-word">authentic</p>
+                                <div class="popup-auth"><p>true to your own personality, values, and spirit, regardless of the pressure that you're under to act otherwise.</p></div>
+                            </div>
+                            <div class="text-right">
+                                <p class="home-word">passionate</p>
+                            </div>
+                        </div>
+                        <div class="w-40 home-venn">
+                            <img class="w-100" src="/img/Earnest Venn Diagram Only.png" alt="">
+                        </div>
+                        <div class="w-30 flex flex-col space-around">
+                            <div class="w-20">
+                                <p class="home-word">mindful</p>
+                            </div>
+                            <div class="w-20">
+                                <p class="home-word">diligent</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <p class="home-word">principled</p>
+                    </div>
+                </div>
             </div>
-            <div class="w-25 text-right">
+            <div class="w-20 text-right">
                 <p>celebrating earnest</p>
                 <p>an underrated word</p>
                 <p>describing a gravity of traits</p>
